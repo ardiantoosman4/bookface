a31e5e88dc8a6e62d51d5cf474dcb7b59dcd0fca
Ardianto <108719126+ardiantoosman4@users.noreply.github.com>
2023-10-26 09:21:57 +0700

    migration seedin

---
+++ models/profile.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Profile extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Profile.init({
+    UserId: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    imagePath: DataTypes.STRING,
+    bio: DataTypes.STRING,
+    gender: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Profile',
+  });
+  return Profile;
+};
